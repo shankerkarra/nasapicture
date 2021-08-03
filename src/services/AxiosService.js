2ded0e8e8c29a4136a9875e3ae98aa8215626319
Shanker Karra <shankerkarra@gmail.com>
2021-08-03 17:04:34 -0600

    check in

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-//import { baseURL } from '../env'
+// import { baseURL } from '../env'
 export const api = Axios.create({
   baseURL: 'https://api.nasa.gov/planetary',
   timeout: 8000
--- src/services/PicturesSearch.js
+++ src/services/PicturesSearch.js
@@ -2,12 +2,9 @@ import { AppState } from '../AppState'
 import { api } from './AxiosService'
 
 class PicturesService {
-
   async getPicture(query = Date()) {
     try {
-      console.log(query)
-      console.log('&date=' + query)
-      const res = await api.get('apod?api_key=7e9kV2zqQmZXQUnao2mE34GrN9xUIrIhKPtyEvUe&date=' + query)
+         const res = await api.get('apod?api_key=7e9kV2zqQmZXQUnao2mE34GrN9xUIrIhKPtyEvUe&date=' + query)
       console.log(res.data)
     } catch (error) {
       console.error(error)
